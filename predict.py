02bad486bb71f6712eb19052bc465fcf3cb9f316
Rishabh Gaur <43523460+Rishabhgaur1998@users.noreply.github.com>
2022-06-12 22:10:54 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,5 +10,5 @@
 # -1,-1, -1,3,3,7,-1,-1,-1,-1,-1,-1,-1,3,3,3,-1,3,-1,-1,-1,-1,3,3, -1,3,-1,5,5,5,5,5,5, 5, -1, -1, 3, 3, -1, -1, -1, 1,
 #  0, 0,22]]
 
-# print("HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",model.predict(r)
+# print(model.predict(r)
 # )
